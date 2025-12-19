780d1f901f47f8609d6bf0cf46194d2ed8f30366
Eng Khaled <Eng Khaled@DESKTOP-9QN4FUV>
2025-12-19 22:03:36 +0200

    a

---
+++ ProductCatalogService/Features/CategoryFeatures/CategoryEndpoints.cs
@@ -0,0 +1,13 @@
+﻿using ProductCatalogService.Features.CategoryFeatures.Queries.ExploreCategories;
+
+namespace ProductCatalogService.Features.CategoryFeatures
+{
+    public static class CategoryEndpoints
+    {
+        public static IEndpointRouteBuilder mapCategoryEndpoints(this IEndpointRouteBuilder app) 
+        {
+            app.MapGetAllCategoriesEndpoint();
+            return app;
+        }
+    }
+}
---
+++ ProductCatalogService/Features/CategoryFeatures/Queries/ExploreCategories/GetAllCategoriesEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using ProductCatalogService.Features.ProductFeatures.Commands.AddProducTOCart;
+
+namespace ProductCatalogService.Features.CategoryFeatures.Queries.ExploreCategories
+{
+    public static class GetAllCategoriesEndpoint
+    {
+        public static IEndpointRouteBuilder MapGetAllCategoriesEndpoint(this IEndpointRouteBuilder app)
+        {
+            app.MapPost("/Categoy/GetAll", async ( IMediator mediator) =>
+            {
+                var response = await mediator.Send(new GetAllCategoriesQuery());
+
+                if (!response.Success)
+                {
+                    return Results.Problem(
+                        detail: string.Join(" | ", response.Errors.Any() ? response.Errors : new[] { response.Message ?? "" }),
+                        statusCode: response.StatusCode
+                    );
+                }
+
+                return Results.Ok(response);
+
+            });
+            return app;
+
+        }
+    }
+}
